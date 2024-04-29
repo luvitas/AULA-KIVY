e1d949f2743197f740ff463509a9e0a519f7e180
samis1207 <162142559+luvitas@users.noreply.github.com>
2024-04-29 12:16:03 -0300

    Add files via upload

--- KIVY2.py
+++ KIVY2.py
@@ -7,5 +7,9 @@ class MyApp(App):
         return Label(text= "Olá SENAI", font_size=100, font_name='Arial', color=get_color_from_hex("#FF5733"))
 halign='left' , # alinha o texto à esquerda 
 valign= 'top' , # alinha o texto no topo
+size_hint=(None,None), # Desativa o ajuste automático do tamanho
+size=(150,50), # Define o tamanho do rótulon
+text_size=(150,None) # Define a largura máxima do texto
+
 if __name__ == '__main__':
     MyApp().run()    
